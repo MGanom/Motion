d9a668c825ae748e840e0cca56a58d1d357c0ff0
MGanom <chewy111@naver.com>
2021-11-04 23:13:53 +0900

    add: page item component, interfaces; refactor: app.ts

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -23,7 +23,8 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
   }
 
   private toEmbeddedURL(url: string): string {
-    const regExp = /^/;
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-_]{11}))|(?:youtu.be\/([a-zA-Z0-9-_]{11})))/;
     const match = url.match(regExp);
 
     const videoId = match ? match[1] || match[2] : undefined;
